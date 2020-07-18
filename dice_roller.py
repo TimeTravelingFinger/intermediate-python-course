3bd4aaf0db467ba7927bfed613a82fab2631061a
TimeTravelingFinger <zbossart@gmail.com>
2020-07-18 16:28:38 -0400

    updated dice roller to roll two dice and give the total.

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,14 @@
-def main():
-  print('You rolled a die')
+import random
+dice_sum = 0
+dice_roll = 2
+#"for every index in a range starting after 0 until the value
+#is equal to dice_rolls, do this:"
+for i in range (0,dice_roll):
+# def main():
+    roll = random.randint(1,6)
+    dice_sum += roll # a shorter way of writing this: dice_sum = dice_sum + roll
+    print(f'You rolled a {roll}')
+print(f'You have rolled a total of {dice_sum}')
 
-if __name__== "__main__":
-  main()
+# if __name__== "__main__":
+#   main()
